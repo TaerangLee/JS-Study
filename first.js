836bc20bc821f89001df58f3497f20aace2a3fc7
TaerangLee <s22051@gsm.hs.kr>
2022-06-29 23:28:04 +0900

    three

--- first.js
+++ first.js
@@ -1,5 +1,4 @@
 console.log('js start')
-
 console.log('first study')
 
 var a=100
@@ -8,3 +7,24 @@ var c='Hello'
 
 console.log(c, a);
 console.log(a)
+
+console.log('')
+var taerang='Hello World'
+
+let name = 'taerang'
+//상수를 선언
+let a =1;
+a = a+3;
+a+=3;
+
+//대입 연산자
+const a= "안녕";
+const b="하세요";
+
+console.log(a+b);
+
+
+
+
+
+
